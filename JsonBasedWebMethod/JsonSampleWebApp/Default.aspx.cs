61d4704549b2105c5909f0360f58d7da8ed60fda
Abijeet Patro <abijeetpatro@gmail.com>
2013-08-10 00:16:52 +0530

    First Project Commit
    JSON based web method commit.

---
+++ JsonBasedWebMethod/JsonSampleWebApp/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JsonSampleWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [System.Web.Services.WebMethod]
+        public static Contact getContact(Contact cnt)
+        {
+            cnt.name = "Abijeet Patro";
+            cnt.phone = "Blah Blah";
+            return cnt;
+        }
+    }
+}
